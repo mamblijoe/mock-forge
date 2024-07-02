004ace5c4dccb8a23fdff0a4390016b4afa1ab02
anton.mambli <anton.mambli@gmail.com>
2024-07-02 22:11:37 +0300

    генерация объектов

--- src/domain/Forge/index.ts
+++ src/domain/Forge/index.ts
@@ -23,9 +23,12 @@ class Forge {
         )
     }
 
-    public forge(configuration: ForgeSchema | ForgeSchema[]): unknown {
+    public forge(
+        configuration: ForgeSchema | ForgeSchema[],
+        count: number = 0
+    ): unknown {
         if (Array.isArray(configuration)) {
-            return this.forgeComplex(configuration)
+            return this.forgeComplex(configuration, count)
         }
 
         return this.forgePrimitive(configuration)
@@ -36,65 +39,32 @@ class Forge {
             return this.forgeArrayOfPrimitives(configuration)
         }
 
-        return this.forgeSinglePrimitive(configuration)
-    }
-
-    private forgeSinglePrimitive(configuration: ForgeSchema) {
         return this.forgeByPrint(configuration)
     }
 
-    private forgeComplex(configuration: ForgeSchema[]) {
+    private forgeComplex(configuration: ForgeSchema[], count: number = 0) {
+        if (count) {
+            return Array(count)
+                .fill({})
+                .map(() => this.forgeSingleObject(configuration))
+        }
+
+        return this.forgeSingleObject(configuration)
+    }
+
+    private forgeSingleObject(configuration: ForgeSchema[]) {
         return configuration.reduce(
             (acc, config) => {
                 const name = config?.key || ''
                 return {
                     ...acc,
-                    [name]: this.forge([config]),
+                    [name]: this.forge(config),
                 }
             },
             {} as Record<string, unknown>
         )
     }
-    //
-    // public forgeArrayOfObjects(configuration: ForgeSchema[], count: number) {
-    //     const example = configuration?.[0] as ForgeSchema
-    //     const isObject = !!example?.meta?.name
-    //
-    //     if (isObject) {
-    //         const emptyArrayFromCount = Array(count).fill({})
-    //         return emptyArrayFromCount.map(() =>
-    //             configuration.reduce((acc, config) => {
-    //                 const name = config?.meta?.name || ''
-    //                 return {
-    //                     ...acc,
-    //                     [name]: this.forgeEntity(config),
-    //                 }
-    //             }, {})
-    //         )
-    //     }
-    //
-    //     return this.forgeArrayOfPrimitives(example, count)
-    // }
-    //
-    // public forgeTupleOfObjects(configuration: ForgeSchema[][]) {
-    //     const example = (configuration as ForgeSchema[][])?.[0]?.[0]
-    //     const isObject = !!example?.meta?.name
-    //
-    //     if (isObject) {
-    //         return configuration.map((item) => {
-    //             return item.reduce((acc, config) => {
-    //                 const name = config?.meta?.name || ''
-    //                 return {
-    //                     ...acc,
-    //                     [name]: this.forgeEntity(config),
-    //                 }
-    //             }, {})
-    //         })
-    //     }
-    //
-    //     return this.forgeTupleOfPrimitives(configuration as ForgeSchema[])
-    // }
-    //
+
     private forgeArrayOfPrimitives(configuration: ForgeSchema) {
         if (!configuration?.meta?.count) {
             throw new Error('Count is not provided')
@@ -105,48 +75,6 @@ class Forge {
             .map(() => this.forgeByPrint(configuration))
     }
 
-    // public forgeTupleOfPrimitives(configuration: ForgeSchema) {
-    //     console.log('INVOKE: forgeTupleOfPrimitives')
-    //     return configuration.map((config) => this.forgeEntity(config))
-    // }
-    //
-    // private forgeEntity(config: ForgeSchema): unknown {
-    //     console.log('INVOKE: forgeEntity')
-    //
-    //     const hasCount = !!config?.meta?.value?.count
-    //     const hasCollectionType =
-    //         config?.meta?.value?.type &&
-    //         [COUNT_TYPE.ARRAY, COUNT_TYPE.TUPLE].includes(
-    //             config?.meta?.value?.type!
-    //         )
-    //     const isCollection = hasCount || hasCollectionType
-    //     console.log('isCollection', {
-    //         name: config.meta?.name,
-    //         isCollection,
-    //         hasCount,
-    //         hasCollectionType,
-    //         config: JSON.stringify(config),
-    //     })
-    //     if (isCollection) {
-    //         const count = config?.meta?.value?.count || 1
-    //         const items = config?.meta?.value?.items || []
-    //         const isTuple = items?.[0] && Array.isArray(items?.[0])
-    //         console.log('--->', isTuple)
-    //         if (isTuple) {
-    //             return this.forgeTupleOfObjects(items as ForgeSchema[][])
-    //         }
-    //         return this.forgeArrayOfObjects(items as ForgeSchema[], count)
-    //     }
-    //
-    //     const hasItems = !!config?.meta?.value?.items
-    //     if (hasItems) {
-    //         const fields = config.meta!.value?.items!
-    //         return this.subFields(fields)
-    //     }
-    //
-    //     return this.forgeByPrint(config)
-    // }
-    //
     private forgeByPrint(config: ForgeSchema) {
         if (isPrimitive(config?.key, config.value)) {
             const module = config.value.module
@@ -157,21 +85,6 @@ class Forge {
 
         return this.forge(config.value)
     }
-    //
-    // private subFields(subFields: ForgeSchema[]) {
-    //     console.log('INVOKE: subFields', subFields)
-    //     const result = {
-    //         ...subFields.reduce((acc, subField) => {
-    //             const key = subField?.meta?.name || ''
-    //             return {
-    //                 ...acc,
-    //                 [key]: this.forgeSinglePrimitive(subField), // выбирать метод для генерации
-    //             }
-    //         }, {}),
-    //     }
-    //     console.log('result', result)
-    //     return result
-    // }
 }
 
 const forge = new Forge()
--- src/main.ts
+++ src/main.ts
@@ -1,129 +1,67 @@
 import Forge from '~/domain/Forge'
-import { COLLECTION_TYPE } from '~/types/forge'
-import { fa } from '@faker-js/faker'
-
-// console.log(
-//     'SINGLE PRIMITIVE',
-//     Forge.forge({
-//         meta: {
-//             isArray: false,
-//         },
-//         value: {
-//             entity: 'nanoid',
-//             module: 'string',
-//         },
-//     }),
-//     '\n'
-// )
-//
-// console.log(
-//     'ARRAY PRIMITIVE',
-//     Forge.forge({
-//         meta: {
-//             isArray: true,
-//             count: 5,
-//         },
-//         value: {
-//             entity: 'nanoid',
-//             module: 'string',
-//         },
-//     }),
-//     '\n'
-// )
-//
-// console.log(
-//     'TUPLE PRIMITIVE',
-//     Forge.forge([
-//         {
-//             meta: {
-//                 isArray: false,
-//             },
-//             value: {
-//                 entity: 'nanoid',
-//                 module: 'string',
-//             },
-//         },
-//         {
-//             meta: {
-//                 isArray: false,
-//             },
-//             value: {
-//                 entity: 'alpha',
-//                 module: 'string',
-//             },
-//         },
-//     ]),
-//     '\n'
-// )
 
 console.log(
     '=',
-    Forge.forge({
-        meta: { count: 0 },
-        value: {
-            module: 'number',
-            entity: 'float',
-        },
-    }),
+    JSON.stringify(
+        Forge.forge(
+            [
+                {
+                    meta: { count: 0 },
+                    key: 'single_primitive_key',
+                    value: {
+                        module: 'number',
+                        entity: 'float',
+                    },
+                },
+                {
+                    meta: { count: 0 },
+                    key: 'nested_key',
+                    value: [
+                        {
+                            meta: { count: 0 },
+                            key: 'nested_single_primitive_key',
+                            value: {
+                                module: 'number',
+                                entity: 'float',
+                            },
+                        },
+                        {
+                            meta: { count: 5 },
+                            key: 'nested_array_primitive_key',
+                            value: {
+                                module: 'number',
+                                entity: 'float',
+                            },
+                        },
+                    ],
+                },
+                {
+                    meta: { count: 5 },
+                    key: 'array_primitive_key',
+                    value: {
+                        module: 'number',
+                        entity: 'float',
+                    },
+                },
+                {
+                    meta: { count: 5 },
+                    key: 'array_complex_key',
+                    value: [
+                        {
+                            meta: { count: 5 },
+                            key: 'nested_single_complex_key',
+                            value: {
+                                module: 'number',
+                                entity: 'float',
+                            },
+                        },
+                    ],
+                },
+            ],
+            5
+        ),
+        null,
+        2
+    ),
     '\n'
 )
-//
-// console.log(
-//     'ARRAY COMPLEX',
-//     Forge.forge({
-//         meta: {
-//             isArray: true,
-//         },
-//         value: [
-//             {
-//                 meta: {
-//                     isArray: false,
-//                 },
-//                 value: {
-//                     entity: 'nanoid',
-//                     module: 'string',
-//                 },
-//             },
-//         ],
-//     }),
-//     '\n'
-// )
-//
-// console.log(
-//     'TUPLE COMPLEX',
-//     Forge.forge([
-//         {
-//             meta: {
-//                 isArray: false,
-//             },
-//             value: [
-//                 {
-//                     meta: {
-//                         isArray: false,
-//                     },
-//                     value: {
-//                         entity: 'nanoid',
-//                         module: 'string',
-//                     },
-//                 },
-//             ],
-//         },
-//         {
-//             meta: {
-//                 isArray: false,
-//             },
-//             value: [
-//                 {
-//                     meta: {
-//                         isArray: false,
-//                     },
-//                     value: {
-//                         entity: 'nanoid',
-//                         module: 'string',
-//                     },
-//                 },
-//             ],
-//         },
-//     ]),
-//     '\n'
-// )
